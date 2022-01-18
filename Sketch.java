4adc13d6716a9340f1225cc4fa2a118504a627d0
Alexyounggg <alex.young23@ycdsbk12.ca>
2022-01-18 18:54:41 -0500

    Movement of the ship and code for collision

--- Sketch.java
+++ Sketch.java
@@ -1,6 +1,12 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
+
+  int shipX = 0;
+  int shipY = 0;
+  int lives = 3;
+  float[] bulletsX = new float [30];
+
 	
 	
   /**
@@ -8,7 +14,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
   /** 
@@ -17,6 +23,10 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
+
+    for (int i = 0; i < bulletsX.length; i++){
+      bulletsX[i] = random(2000 - width) + width;
+    }
   }
 
   /**
@@ -24,12 +34,40 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    background(32);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    rect(shipX, shipY, width/10, height/20);
+
+    if (keyPressed){
+      if (key == 'w'){
+        shipY -=5;
+      }
+      if (key == 's'){
+        shipY +=5;
+      }
+      if (key == 'a'){
+        shipX -=5;
+      }
+      if (key == 'd'){
+        shipX +=5;
+      }
+    }
+
+    for (int i = 0; i < bulletsX.length; i++){
+      float bulletsY = height * i/bulletsX.length;
+      rect(bulletsX[i], bulletsY, 20, 5);
+
+      bulletsX[i] --;
+
+      for (int j = 0; j < bulletsX.length; j++){
+        if (bulletsX[j] < shipX + width/10 && bulletsY > shipY && bulletsY + 5 < shipY + height/20 ){
+          lives --;
+        }
+      }
+     
+    }
+
+
   }
   
   // define other methods down here.
